1c1e9aaa60a1f4dfa342c731a6eca3858a38d11a
finnbear <finnbearone@gmail.com>
2016-08-15 17:06:29 -0700

    Bug fixes and new edit features.

--- routes/create.js
+++ routes/create.js
@@ -21,18 +21,46 @@ router.post('/', function(req, res) {
   }
   else if (req.body.type == "page")
   {
-    if (utility.isAnyNullOrEmpty([req.body.path, req.body.title, req.body.text, req.body.options, req.body.optionLinks])) {
+    if (utility.isAnyNullOrEmpty([req.body.path, req.body.title, req.body.text])) {
       console.log("Malformed POST to create, missing required fields.");
       res.redirect('view');
     }
     else {
+      console.log("Valid page post")
       var json = {};
       json.title = req.body.title;
       json.text = req.body.text;
       json.options = [];
       try {
-        var options = JSON.parse(req.body.options);
-        var optionLinks = JSON.parse(req.body.optionLinks);
+        var options = [];
+        var optionLinks = [];
+
+        if (req.body.n0 != null && req.body.p0 != null)
+        {
+          options.push(req.body.n0);
+          optionLinks.push(req.body.p0);
+        }
+        if (req.body.n1 != null && req.body.p1 != null)
+        {
+          options.push(req.body.n1);
+          optionLinks.push(req.body.p1);
+        }
+        if (req.body.n2 != null && req.body.p2 != null)
+        {
+          options.push(req.body.n2);
+          optionLinks.push(req.body.p2);
+        }
+        if (req.body.n3 != null && req.body.p3 != null)
+        {
+          options.push(req.body.n3);
+          optionLinks.push(req.body.p3);
+        }
+        if (req.body.n4 != null && req.body.p4 != null)
+        {
+          options.push(req.body.n4);
+          optionLinks.push(req.body.p4);
+        }
+        console.log(options.length);
         for (var i = 0; i < Math.min(options.length, optionLinks.length); i += 1)
         {
           json.options.push({text: options[i], link: optionLinks[i]});
